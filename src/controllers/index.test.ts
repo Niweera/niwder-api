3ac49f50b1987f1655dfc6649a2bad409d860fb0
Nipuna Weerasekara <w.nipuna@gmail.com>
2022-03-29 05:34:02 +0530

    Release v0.1.2

--- src/controllers/index.test.ts
+++ src/controllers/index.test.ts
@@ -2,12 +2,20 @@ import request from "supertest";
 import { app } from "../server";
 import { getIDToken } from "../middleware/firebaser.test";
 
+//const file =
+//"https://mega.nz/file/1wQXkIyR#mTci1QDs5cjg5-rhxjrYU5QxUbN_OLkuuwyYyWvQIyo";
+//const folder = "https://mega.nz/folder/xspkCBpa#a0oa70caa9nvTpufH6bm6g";
+//const folderFolder =
+//"https://mega.nz/folder/xspkCBpa#a0oa70caa9nvTpufH6bm6g/folder/h1pQjY4D";
+const folderFile =
+  "https://mega.nz/folder/xspkCBpa#a0oa70caa9nvTpufH6bm6g/file/txpSSQZZ";
+
 describe("POST /mega-to-gdrive", () => {
   it("Should return HTTP 204", async () => {
     const res: request.Response = await request(app)
       .post("/api/mega-to-gdrive")
       .send({
-        url: `https://mega.nz/folder/xspkCBpa#a0oa70caa9nvTpufH6bm6g`,
+        url: folderFile,
       })
       .set("Authorization", `Bearer ${await getIDToken()}`);
 
--- src/worker/MegaToGDriveWorker.ts
+++ src/worker/MegaToGDriveWorker.ts
@@ -2,7 +2,7 @@ import type { Job } from "bullmq";
 import { db } from "../database";
 import type { database } from "firebase-admin";
 import { ServerValue } from "firebase-admin/database";
-import { existsSync, mkdtempSync, rmSync } from "fs";
+import { existsSync, mkdtempSync, readdirSync, rmSync, statSync } from "fs";
 import * as path from "path";
 import * as os from "os";
 import { File as MEGAFile } from "megajs";
@@ -21,17 +21,29 @@ export default class MegaToGDriveWorker {
 
   private downloadToDisk = async (): Promise<FileObject> => {
     return new Promise(async (resolve, reject) => {
-      try {
-        const megaURL: string = this.job.data.url;
-        console.log(`now downloading ${megaURL}`);
-        const tempDir: string = mkdtempSync(
-          path.join(os.tmpdir(), "niwder-tmp")
-        );
+      const megaURL: string = this.job.data.url;
+      console.log(`now downloading ${megaURL}`);
+
+      const tempDir: string = mkdtempSync(path.join(os.tmpdir(), "niwder-tmp"));
+
+      const fileRe = new RegExp(
+        /^https:\/\/mega\.nz\/file\/[a-zA-Z0-9]{0,8}#[a-zA-Z0-9_-]+$/g
+      );
+      const folderRe = new RegExp(
+        /^https:\/\/mega\.nz\/folder\/[a-zA-Z0-9]{0,8}#[a-zA-Z0-9_-]+$/g
+      );
+      const folderFolderRe = new RegExp(
+        /^https:\/\/mega\.nz\/folder\/[a-zA-Z0-9]{0,8}#[a-zA-Z0-9_-]+\/folder\/[a-zA-Z0-9]{0,8}$/g
+      );
+      const folderFileRe = new RegExp(
+        /^https:\/\/mega\.nz\/folder\/[a-zA-Z0-9]{0,8}#[a-zA-Z0-9_-]+\/file\/[a-zA-Z0-9]{0,8}$/g
+      );
+
+      if (fileRe.test(megaURL) || folderRe.test(megaURL)) {
         const file: MEGAFile = MEGAFile.fromURL(megaURL);
         await file.loadAttributes();
 
         const filePath: string = path.join(tempDir, file.name);
-
         const megaCMD = spawn("mega-get", [`"${megaURL}"`, tempDir]);
 
         megaCMD.stdout.on("data", (data) => {
@@ -60,8 +72,72 @@ export default class MegaToGDriveWorker {
             reject(new Error(`${filePath} is missing`));
           }
         });
-      } catch (e) {
-        reject(e);
+      } else if (folderFolderRe.test(megaURL)) {
+        const megaCMD = spawn("mega-get", [`"${megaURL}"`, tempDir]);
+
+        megaCMD.stdout.on("data", (data) => {
+          console.log(`\x1b[A\x1b[G\x1b[2K${data}`);
+        });
+
+        megaCMD.stderr.on("data", (data) => {
+          console.log(`\x1b[A\x1b[G\x1b[2K${data}`);
+        });
+
+        megaCMD.on("error", (err) => {
+          reject(err);
+        });
+
+        megaCMD.on("close", async (code) => {
+          const downloaded = readdirSync(tempDir);
+          const filePath = path.join(tempDir, downloaded[0]);
+
+          if (existsSync(filePath) && code === 0) {
+            await this.job.updateProgress(49);
+            resolve({
+              fileName: downloaded[0],
+              filePath,
+              fileMimeType: "inode/directory",
+              fileSize: 0,
+              directory: true,
+            });
+          } else {
+            reject(new Error(`${filePath} is missing`));
+          }
+        });
+      } else if (folderFileRe.test(megaURL)) {
+        const megaCMD = spawn("mega-get", [`"${megaURL}"`, tempDir]);
+
+        megaCMD.stdout.on("data", (data) => {
+          console.log(`\x1b[A\x1b[G\x1b[2K${data}`);
+        });
+
+        megaCMD.stderr.on("data", (data) => {
+          console.log(`\x1b[A\x1b[G\x1b[2K${data}`);
+        });
+
+        megaCMD.on("error", (err) => {
+          reject(err);
+        });
+
+        megaCMD.on("close", async (code) => {
+          const downloaded = readdirSync(tempDir);
+          const filePath = path.join(tempDir, downloaded[0]);
+
+          if (existsSync(filePath) && code === 0) {
+            await this.job.updateProgress(49);
+            resolve({
+              fileName: downloaded[0],
+              filePath,
+              fileMimeType: mime.lookup(filePath) || "application/octet-stream",
+              fileSize: statSync(filePath).size,
+              directory: false,
+            });
+          } else {
+            reject(new Error(`${filePath} is missing`));
+          }
+        });
+      } else {
+        reject(new Error("Mega.nz link is not understandable"));
       }
     });
   };
